698aa0785213ac1278c57ff3d4f4d6ef2c138e92
iduk <iduk.design@gmail.com>
2021-12-26 20:08:16 +0900

    [fix] Layout Styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,9 +16,9 @@ export default function Layout() {
   return (
     <>
       <main id="body-pd">
-        <header className="header" id="header">
-          <div className="header_toggle">
-            <i className="bx bx-menu" id="header-toggle" />
+        <header className="topbar" id="topbar">
+          <div className="sidebar_toggle">
+            <i className="bx bx-menu" id="sidebar-toggle" />
           </div>
           <div className="header_img">111</div>
         </header>
@@ -67,27 +67,27 @@ export default function Layout() {
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const showNavbar = (toggleId, navId, bodyId, topbarId) => {
     const toggle = document.getElementById(toggleId),
       nav = document.getElementById(navId),
       bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId) // Validate that all variables exist
+      topbarpd = document.getElementById(topbarId) // Validate that all variables exist
 
-    if (toggle && nav && bodypd && headerpd) {
+    if (toggle && nav && bodypd && topbarpd) {
       toggle.addEventListener('click', () => {
         // show navbar
         nav.classList.toggle('show') // change icon
 
         toggle.classList.toggle('bx-x') // add padding to body
 
-        bodypd.classList.toggle('body-pd') // add padding to header
+        bodypd.classList.toggle('body-pd') // add padding to topbar
 
-        headerpd.classList.toggle('body-pd')
+        topbarpd.classList.toggle('body-pd')
       })
     }
   }
 
-  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+  showNavbar('sidebar-toggle', 'nav-bar', 'body-pd', 'topbar')
   /*===== LINK ACTIVE =====*/
 
   const linkColor = document.querySelectorAll('.nav_link')
