2f7011466b4e1ce5d4b5d511a5f4ed6173b8305d
iduk <iduk.design@gmail.com>
2022-01-05 13:43:43 +0900

    [bugfix] *reload오류해결, BrowserRouter => HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import * as bootstrap from 'bootstrap'
 import './assets/scss/style.scss'
 
 render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
