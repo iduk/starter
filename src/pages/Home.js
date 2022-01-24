22099cbdfde786b3dff7bd06c4142891bd9b7564
iduk <iduk.design@gmail.com>
2022-01-25 00:05:39 +0900

    [fix] Sass Colors & Button 수식 고민중..

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,45 +3,73 @@ import styles from './Home.module.scss'
 import classnames from 'classnames/bind'
 import Loading from '../components/Loading'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const cx = classnames.bind(styles)
+
+const ThemeWrap = styled.div`
+  height: 280px;
+  padding: 3rem 2rem;
+  margin-top: 2rem;
+  &[data-theme='dark'] {
+    background-color: var(--bg);
+    color: var(--color);
+    border: 1px solid var(--border);
+  }
+  &[data-theme='light'] {
+    background-color: var(--bg);
+    color: var(--color);
+    border: 1px solid var(--border);
+  }
+`
 
 function Home() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2000)
+    setTimeout(() => setLoading(false), 0)
   }, [])
 
   return (
     <>
       {loading === false ? (
         <div className={cx('home')}>
-          <article
+          <ThemeWrap
+            data-theme="light"
             className="flex flex-col justify-center text-center"
-            style={{ height: '50vh' }}
           >
-            <h5>Welcome, Pictor World!</h5>
-            <p>
-              Nowadays, websites have begun using loading screens that are more
-              in tune with the platform’s vision: If they fall back to
-              platitudinous progress bars, at least they spice it up with the
-              product’s general aesthetic.
-            </p>
-            <div className="mt-5">
-              <Link
-                to="/"
-                className="btn p-3 text-white"
-                style={{ backgroundColor: 'var(--primary)' }}
-              >
-                Go Home
-              </Link>
-            </div>
-          </article>
+            <h6>Theme Mode: Light</h6>
+            <HomeContent />
+          </ThemeWrap>
+
+          <ThemeWrap
+            data-theme="dark"
+            className="flex flex-col justify-center text-center"
+          >
+            <h6>Theme Mode: Dark</h6>
+            <HomeContent />
+          </ThemeWrap>
         </div>
       ) : (
         <Loading />
       )}
+    </>
+  )
+}
+
+function HomeContent() {
+  return (
+    <>
+      <h5>Welcome, Pictor World</h5>
+
+      <p>
+        별빛이 이 가을 못 슬퍼하는 버리었습니다. 청춘이 강아지, 패, 계십니다.
+        남은 풀이 옥 내린 노새, 아름다운 못 봅니다.
+      </p>
+      <div className="mt-5 flex gap-2 justify-center">
+        <button className="btn btn-primary p-2 ">primary</button>
+        <button className="btn btn-secondary p-2">secondary</button>
+      </div>
     </>
   )
 }
