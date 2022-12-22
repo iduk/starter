56edd67ce45de6d8d30a4d838e4f4b9688e1daf3
iduk2 <iduk2@cj.net>
2022-12-22 23:17:55 +0900

    대응실패 후.. runtime 에러 플러그인 설치222

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
